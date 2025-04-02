6394661ed7fca8bb081b40d64f8b947784f320e5
Elad Nava <eladnava@gmail.com>
2025-04-02 11:36:12 -0300

    Root is no longer needed (changed build.props instead) with Magisk

--- app/src/main/java/com/wifidev/logic/adb/TCPIP.java
+++ app/src/main/java/com/wifidev/logic/adb/TCPIP.java
@@ -11,77 +11,80 @@ public class TCPIP
 {
     public static void set()
     {
-        //--------------------------------
-        // Make sure we have root
-        //--------------------------------
-
-        if (!RootTools.isRootAvailable())
-        {
-            return;
-        }
-
-        //--------------------------------
-        // Make sure we have root
-        // Also asks for root if not granted
-        //--------------------------------
-
-        if (!RootTools.isAccessGiven())
-        {
-            return;
-        }
-
-        //--------------------------------
-        // Log it
-        //--------------------------------
-
-        Log.d(Logging.TAG_NAME, "Restarting adbd in tcpip mode..." );
-
-        //--------------------------------
-        // Set tcp port of adbd
-        // And restart daemon
-        //--------------------------------
-
-        String command = "stop adbd && setprop service.adb.tcp.port " + ADB.TCPIP_PORT + " && start adbd &";
-
-        try
-        {
-            //--------------------------------
-            // Execute the command
-            //--------------------------------
-
-            RootCommand.execute(command);
-        }
-        catch( Exception exc )
-        {
-            //--------------------------------
-            // Log exception
-            //--------------------------------
-
-            Log.e(Logging.TAG_NAME, exc.getMessage());
-        }
-
-        //--------------------------------
-        // Enable Wi-Fi tethering
-        // regardless of carrier decision
-        //--------------------------------
-
-        command = "setprop net.tethering.noprovisioning true";
-
-        try
-        {
-            //--------------------------------
-            // Execute the command
-            //--------------------------------
-
-            RootCommand.execute(command);
-        }
-        catch( Exception exc )
-        {
-            //--------------------------------
-            // Log exception
-            //--------------------------------
-
-            Log.e(Logging.TAG_NAME, exc.getMessage());
-        }
+        // Do nothing (we do this in props now)
+        return;
+//
+//        //--------------------------------
+//        // Make sure we have root
+//        //--------------------------------
+//
+//        if (!RootTools.isRootAvailable())
+//        {
+//            return;
+//        }
+//
+//        //--------------------------------
+//        // Make sure we have root
+//        // Also asks for root if not granted
+//        //--------------------------------
+//
+//        if (!RootTools.isAccessGiven())
+//        {
+//            return;
+//        }
+//
+//        //--------------------------------
+//        // Log it
+//        //--------------------------------
+//
+//        Log.d(Logging.TAG_NAME, "Restarting adbd in tcpip mode..." );
+//
+//        //--------------------------------
+//        // Set tcp port of adbd
+//        // And restart daemon
+//        //--------------------------------
+//
+//        String command = "stop adbd && setprop service.adb.tcp.port " + ADB.TCPIP_PORT + " && start adbd &";
+//
+//        try
+//        {
+//            //--------------------------------
+//            // Execute the command
+//            //--------------------------------
+//
+//            RootCommand.execute(command);
+//        }
+//        catch( Exception exc )
+//        {
+//            //--------------------------------
+//            // Log exception
+//            //--------------------------------
+//
+//            Log.e(Logging.TAG_NAME, exc.getMessage());
+//        }
+//
+//        //--------------------------------
+//        // Enable Wi-Fi tethering
+//        // regardless of carrier decision
+//        //--------------------------------
+//
+//        command = "setprop net.tethering.noprovisioning true";
+//
+//        try
+//        {
+//            //--------------------------------
+//            // Execute the command
+//            //--------------------------------
+//
+//            RootCommand.execute(command);
+//        }
+//        catch( Exception exc )
+//        {
+//            //--------------------------------
+//            // Log exception
+//            //--------------------------------
+//
+//            Log.e(Logging.TAG_NAME, exc.getMessage());
+//        }
     }
 }
